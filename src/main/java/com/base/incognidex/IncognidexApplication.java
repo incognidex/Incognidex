71fd9bb2d3edccc0b724490f5bdc9fbb0ee94d46
ParfaitCMD <devparfaitdev20@gmail.com>
2025-08-13 18:54:45 -0300

    Enviando base

---
+++ src/main/java/com/base/incognidex/IncognidexApplication.java
@@ -0,0 +1,13 @@
+package com.base.incognidex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncognidexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncognidexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/incognidex/IncognidexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.incognidex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncognidexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
