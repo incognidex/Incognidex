b50e00d8b9ce8d28a8be07feccd1f87d0f4c3040
Rhawan-H <rhawan.henrique18@gmail.com>
2025-08-28 21:48:07 -0300

    update: atualização 1.0

--- backend/src/main/java/com/incognidex/base/Controller/UsersController.java
+++ backend/src/main/java/com/incognidex/base/Controller/UsersController.java
@@ -1,4 +1,26 @@
 package com.incognidex.base.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.incognidex.base.Service.*;
+import com.incognidex.base.model.UsuarioModel;;
 
+public class UsersController {
+ @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/editar")
+    public String exibirPaginaEdicao(Model model) {
+
+        Object usuarioIdLogado = null;
+        UsuarioModel usuario = UsuarioModel.buscarPorId(usuarioIdLogado);
+
+        model.addAttribute("usuario", usuario);
+        
+        return "pagina-de-edicao";
+    }
+}
 
 
