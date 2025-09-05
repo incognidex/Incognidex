4b221a6959250a9c189e59105f9d316a49db58fa
ParfaitCMD <devparfaitdev20@gmail.com>
2025-09-04 22:09:53 -0300

    Update: Corrigindo uma leve identação

--- backend/src/main/java/com/incognidex/base/Controller/UsersController.java
+++ backend/src/main/java/com/incognidex/base/Controller/UsersController.java
@@ -1,16 +1,21 @@
 package com.incognidex.base.controller; // Pacote em minúsculas
 
 // Importações do Spring Framework
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-// Importações com pacotes em minúsculas
-import com.incognidex.base.model.UsuarioModel;
-import com.incognidex.base.service.UsuarioService;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.incognidex.base.model.UsuarioModel;
+import com.incognidex.base.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -33,7 +38,7 @@ public class UsersController {
     public ResponseEntity<UsuarioModel> buscarUsuarioPorId(@PathVariable Integer id) {
         Optional<UsuarioModel> usuario = usuarioService.buscarPorId(id);
         return usuario.map(ResponseEntity::ok)
-                      .orElse(ResponseEntity.notFound().build());
+                        .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
