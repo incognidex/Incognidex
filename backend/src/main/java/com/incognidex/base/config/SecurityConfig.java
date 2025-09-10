6c011fb24c24eeeadbe257b786c1672c1e9cbe22
ParfaitCMD <devparfaitdev20@gmail.com>
2025-09-10 14:25:14 -0300

    Update: Corrigindo localização

--- backend/src/main/java/com/incognidex/base/config/SecurityConfig.java
+++ backend/src/main/java/com/incognidex/base/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.incognidex.Config;
+package com.incognidex.base.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
