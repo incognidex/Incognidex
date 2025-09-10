9448fdfcf200cb3a779c850d40b1a673384cd0ce
ParfaitCMD <devparfaitdev20@gmail.com>
2025-09-10 15:13:48 -0300

    Update: Configurando Cors

---
+++ backend/src/main/java/com/incognidex/base/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.incognidex.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "file://")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
